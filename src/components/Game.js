0351ba9ae1adef6468433565b2e208ca8de03fd0
Ruslan <ruslanchikbest@gmail.com>
2023-09-26 14:06:22 +0300

    Create states board, xlsNext, import calculateWinner

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import Board from './Board';
+import { calculateWinner } from './helper';
 
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xlsNext, setXlsNext] = useState(true);
+  const winner = calculateWinner(board);
   return (
     <div className='game__wrapper'>
       <Board />
