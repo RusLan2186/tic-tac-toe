75fd2a06dc298d20b88e14674f3d059483b8eba0
Ruslan <ruslanchikbest@gmail.com>
2023-09-26 14:10:05 +0300

    Create function handleClick, transfer props to Board

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,9 +7,23 @@ const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xlsNext, setXlsNext] = useState(true);
   const winner = calculateWinner(board);
+  const handleClick = (index) => {
+    const boardCopy = [...board];
+    // Определить был ли клик по ячейке или игра закончена
+    if (winner || boardCopy[index]) return;
+    console.log(boardCopy);
+
+    // Определить чей ход Х или 0
+    boardCopy[index] = xlsNext ? 'X' : '0';
+
+    // Обновить наш стейт
+    setBoard(boardCopy);
+    setXlsNext(!xlsNext);
+  };
+
   return (
     <div className='game__wrapper'>
-      <Board />
+      <Board squares={board} />
     </div>
   );
 };
