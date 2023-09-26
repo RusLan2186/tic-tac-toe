9308c1738360b2fba1394f813f86b657c054789d
Ruslan <ruslanchikbest@gmail.com>
2023-09-26 14:45:48 +0300

    Adding game score and winner information

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Board from './Board';
 import { calculateWinner } from './helper';
 
@@ -15,7 +15,6 @@ const Game = () => {
     const boardCopy = [...board];
     // Определить был ли клик по ячейке или игра закончена
     if (winner || boardCopy[index]) return;
-    console.log(boardCopy);
 
     // Определить чей ход Х или 0
     boardCopy[index] = xIsNext ? 'X' : '0';
@@ -32,10 +31,40 @@ const Game = () => {
     );
   };
 
+  useEffect(() => {
+    if (board.includes(null) || winner) {
+      setDeadHead(null);
+    } else {
+      setDeadHead(true);
+    }
+  }, [winner, board]);
+
+  useEffect(() => {
+    if (winner === 'X') {
+      setWinnerX(winnerX + 1);
+    }
+    if (winner === '0') {
+      setWinner0(winnerO + 1);
+    }
+    if (deadHead === true) {
+      setDeadHeadQuantity(deadHeadQuantity + 1);
+    }
+  }, [winner, deadHead]);
+
   return (
     <div className='game__wrapper'>
       {startNewGame()}
       <Board squares={board} click={handleClick} />
+      {deadHead ? (
+        <h2 className='dead__head'>dead heat</h2>
+      ) : (
+        <div className='game__info'>
+          {winner ? 'Winner' + ' ' + winner : 'is walking now ' + (xIsNext ? 'X' : '0')}
+          <p> Winner X {winnerX}</p>
+          <p> Winner 0{winnerO}</p>
+          <p>DeadHead{deadHeadQuantity}</p>
+        </div>
+      )}
     </div>
   );
 };
